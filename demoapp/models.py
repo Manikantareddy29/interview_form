92df1a74685575cb1d5b4bd407a2c298ea4077bf
Manikantareddy29 <manikantareddyasam29@gmail.com>
2023-11-07 14:50:20 +0530

    added files to git

---
+++ demoapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    #image
+    image = models.ImageField(upload_to= "images/")
+    #video 
+    video = models.FileField(upload_to= "media/")
+    description = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return self.title
+
